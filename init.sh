f7a51cfc9c0e953f180e862287168e01b04ea1d3
Carlos Marcelo <marcelo@voidspace.us>
2022-02-12 19:13:31 +0000

    Changed config name

--- init.sh
+++ init.sh
@@ -9,12 +9,10 @@ kubectl create namespace infra
 gpg --export-secret-keys --armor "9DC11809DD7D102342B09932C40921F65EFEF866" | kubectl create secret generic sops-gpg --namespace=flux-system --from-file=sops.asc=/dev/stdin
 gpg --export-secret-keys --armor "9DC11809DD7D102342B09932C40921F65EFEF866" | kubectl create secret generic sops-gpg --namespace=infra --from-file=sops.asc=/dev/stdin
 
-kubectl apply -f kube-router.yaml
-
-#flux bootstrap github \
-#  --owner=ZeroNull7 \
-#  --repository=kind-k8s \
-#  --path=k8s \
-#  --branch=main \
-#  --cluster-domain dev.local \
-#  --personal
+flux bootstrap github \
+  --owner=ZeroNull7 \
+  --repository=kind-k8s \
+  --path=k8s \
+  --branch=main \
+  --cluster-domain dev.local \
+  --personal
